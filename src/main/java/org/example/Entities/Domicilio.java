17ff7d902aea7f51b15a9133da023a8a91e7eb14
erredev-JS <roddev100@gmail.com>
2024-05-21 16:11:36 -0300

    Adding Domicilio entitie.

---
+++ src/main/java/org/example/Entities/Domicilio.java
@@ -0,0 +1,59 @@
+package org.example.Entities;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Domicilio")
+public class Domicilio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    // Atributos
+
+    private String calle;
+    private Integer numero;
+    private Integer cp;
+
+    // Constructores
+
+
+    public Domicilio(String calle, Integer numero, Integer cp) {
+        this.calle = calle;
+        this.numero = numero;
+        this.cp = cp;
+    }
+
+
+    // Getter´s and Setter´s
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Integer getCp() {
+        return cp;
+    }
+
+    public void setCp(Integer cp) {
+        this.cp = cp;
+    }
+}
