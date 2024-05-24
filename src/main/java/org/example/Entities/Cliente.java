2a5a6581aa22f47de95790d0663731ffd4c2a2c7
erredev-JS <roddev100@gmail.com>
2024-05-24 18:22:40 -0300

    Fixed cliente entity

--- src/main/java/org/example/Entities/Cliente.java
+++ src/main/java/org/example/Entities/Cliente.java
@@ -6,6 +6,9 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "Clientes")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     //Atributos
     private String nombre;
     private String apellidp;
@@ -13,12 +16,86 @@ public class Cliente {
     private String email;
     private LocalDate fechaNacimiento;
 
-    //Relaciones
+    // Relaciones
     @OneToOne(cascade = CascadeType.ALL)
     private UsuarioCliente usuario;
     @OneToOne(cascade = CascadeType.ALL)
     private ImagenCliente imagenCliente;
 
+    // Constructores
 
 
+    public Cliente(String nombre, String apellidp, String telefono, String email, LocalDate fechaNacimiento, UsuarioCliente usuario, ImagenCliente imagenCliente) {
+        this.nombre = nombre;
+        this.apellidp = apellidp;
+        this.telefono = telefono;
+        this.email = email;
+        this.fechaNacimiento = fechaNacimiento;
+        this.usuario = usuario;
+        this.imagenCliente = imagenCliente;
+    }
+
+
+    // Getter´s and Setter´s
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidp() {
+        return apellidp;
+    }
+
+    public void setApellidp(String apellidp) {
+        this.apellidp = apellidp;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public UsuarioCliente getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(UsuarioCliente usuario) {
+        this.usuario = usuario;
+    }
+
+    public ImagenCliente getImagenCliente() {
+        return imagenCliente;
+    }
+
+    public void setImagenCliente(ImagenCliente imagenCliente) {
+        this.imagenCliente = imagenCliente;
+    }
 }
